f79b81101a136067e8e64783c3a06d3994d982c8
nakanotti <nakanotti@gmail.com>
2024-05-29 13:05:21 +0900

    "/nicklist uuids" コマンドで、UUID 込みのオンラインプレイヤーの一覧を出力する。

--- src/main/java/ml/noahc3/nickafkpartypack/Commands/CommandNickList.java
+++ src/main/java/ml/noahc3/nickafkpartypack/Commands/CommandNickList.java
@@ -10,6 +10,13 @@ import org.bukkit.entity.Player;
 public class CommandNickList implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0 && args[0].equals("uuids")) {
+            for(Player p : Bukkit.getOnlinePlayers()) {
+                sender.sendMessage(p.getUniqueId().toString() + " " + p.getName() + " " + (Tasks.isPlayerNicked(p) ? Tasks.getPlayerDisplayName(p) : ""));
+            }
+            return true;
+        }
+
         sender.sendMessage("Nicked Players ([real name] > [nickname])");
 
         for(Player p : Bukkit.getOnlinePlayers()) {
